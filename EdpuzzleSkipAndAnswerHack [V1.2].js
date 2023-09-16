234d2eac6ad526cf4db15ff932462f94f80c3ca3
LeoChengSC <105137161+LeoChengSC@users.noreply.github.com>
2023-09-16 18:14:15 +0800

    EdpuzzleSkipAndAnswerHack [V1.2].js

--- EdpuzzleSkipAndAnswerHack [V1.2].js
+++ EdpuzzleSkipAndAnswerHack [V1.2].js
@@ -193,7 +193,7 @@ function openPopup(assignment) {
     <p style="font-size: 12px" id="loading_text"></p>
   </div>
   <hr>
-  <p style="font-size: 12px">Made by: <a target="_blank" href="https://github.com/LeoChengSC">LeoChengSC</a> on Github | Website: <a target="_blank" href="https://edpuzzle.hs.vc">edpuzzle.hs.vc</a> | Source code: <a target="_blank" href="https://github.com/LeoChengSC/Hack/blob/main/EdpuzzleHack.js">LeoChengSC/EdpuzzleHack</a></p>
+  <p style="font-size: 12px">Made by: <a target="_blank" href="https://github.com/LeoChengSC">LeoChengSC</a> on Github | Website: <a target="_blank" href="https://edpuzzle.hs.vc">edpuzzle.hs.vc</a> | Source code: <a target="_blank" href="https://github.com/LeoChengSC/Hack/tree/main"</a></p>
   popup = window.open("about:blank", "", "width=600, height=400");
   popup.document.write(base_html);
 
